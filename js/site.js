7dd33b621fea19d062ef34c0a1f25aba1fb02d22
Rosa Cabrera <rcabrer1@hawk.iit.edu>
2016-12-04 15:04:55 -0600

    Added missig braces

--- js/site.js
+++ js/site.js
@@ -1,14 +1,16 @@
 // TODO: Use a function closure and release global $
 $.noConflict();
-$(document).ready(function() {
-  $('#bmi-form').on('submit', function(event) {
-    var endpoint = $('https://bmi.p.mashape.com/');
-    var weight = $('#bmi-weight');
-    var height = $('#bmi-height');
-    var key = $('WyFUMDOkdrmshARoxfXDWLZmMeccp180tJEjsnCz3MCFuXJdEo')    
-    var url = endpoint + weight + height + key;
-    
-    $.get(url);
-    event.preventDefault();
+(function($){
+  $(document).ready(function() {
+    $('#bmi-form').on('submit', function(event) {
+      var endpoint = $('https://bmi.p.mashape.com/');
+      var weight = $('#bmi-weight');
+      var height = $('#bmi-height');
+      var key = $('WyFUMDOkdrmshARoxfXDWLZmMeccp180tJEjsnCz3MCFuXJdEo')    
+      var url = endpoint + weight + height + key;
+      
+      $.get(url);
+      event.preventDefault();
+    });
   });
 });
