f50c939a1d460414430d85bb08af290fdaaf4063
Rosa Cabrera <rcabrer1@hawk.iit.edu>
2016-12-04 15:01:57 -0600

    Changed js file to submit and get bmi data

--- js/site.js
+++ js/site.js
@@ -1,19 +1,14 @@
 // TODO: Use a function closure and release global $
+$.noConflict();
 $(document).ready(function() {
   $('#bmi-form').on('submit', function(event) {
     var endpoint = $('https://bmi.p.mashape.com/');
     var weight = $('#bmi-weight');
     var height = $('#bmi-height');
     var key = $('WyFUMDOkdrmshARoxfXDWLZmMeccp180tJEjsnCz3MCFuXJdEo')    
+    var url = endpoint + weight + height + key;
     
-    $.get(function(data) {
-        $('#bmi').append(
-          var url = endpoint + weight + height + key;
-          loadJSON(url,gotData)
-        );
-      });
+    $.get(url);
     event.preventDefault();
   });
 });
-
-/*xhr.setRequestHeader("X-Mashape-Authorization", "WyFUMDOkdrmshARoxfXDWLZmMeccp180tJEjsnCz3MCFuXJdEo");*/
