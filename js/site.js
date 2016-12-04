51af3d01876f00a739cc0cdc9ad688d7c10e1005
Rosa Cabrera <rcabrer1@hawk.iit.edu>
2016-12-04 15:59:17 -0600

    Trying different function, again

--- js/site.js
+++ js/site.js
@@ -1,11 +1,20 @@
-$.ajax({
-    url: 'https://bmi.p.mashape.com/', // The URL to the API. You can get this in the API page of the API you intend to consume
-    type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
-    data: {weight, height}, // Additional parameters here
-    dataType: 'json',
-    success: function(data) { console.dir((data.source)); },
-    error: function(err) { alert(err); },
-    beforeSend: function(xhr) {
-    xhr.setRequestHeader("X-Mashape-Authorization", "WyFUMDOkdrmshARoxfXDWLZmMeccp180tJEjsnCz3MCFuXJdEo"); // Enter here your Mashape key
-    }
-})
+// TODO: Use a function closure and release global $
+$.noConflict();
+(function($){
+  $(document).ready(function() {
+    $('#bmi-form').on('submit', function(event) {
+      //saving inputs
+      var weight = $('#bmi-weight').val();
+      var height = $('#bmi-height').val();
+      
+      $.get(
+          'https://bmi.p.mashape.com/WyFUMDOkdrmshARoxfXDWLZmMeccp180tJEjsnCz3MCFuXJdEo' + weight + height,
+          function(data){
+            $('#bmi').append(
+            '<p>Your BMI is:</p>)'+ data.bmi;
+          }
+        );
+    });
+  event.preventDefault();
+  });
+});
