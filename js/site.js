948356cf666c4b28eddccef35bed33981c2f86ff
Rosa Cabrera <rcabrer1@hawk.iit.edu>
2016-12-07 16:51:23 -0600

    Changed output for bmi

--- js/site.js
+++ js/site.js
@@ -21,8 +21,10 @@ $.noConflict();
            '<p>Your risk is: '+data.bmi.risk+'</p>'
           );
       });  
-        var string= ‘Your Bmi: ‘+value+‘. Your status:’ '+status+' ‘. Your risk:’ '+risk+' ‘.’'
-        $(‘#results’).text(string);
+
+        var string= 'Your Bmi: ' +value+ '. Your status:' +status+ '. Your risk:' +risk + '.';
+        $('#results').text(string);
+
 
     });
   event.preventDefault();
