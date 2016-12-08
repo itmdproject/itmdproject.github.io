81d3b60f54dbd8f1f649c52e77e66d8d13af8a12
Rosa Cabrera <rcabrer1@hawk.iit.edu>
2016-12-07 23:57:32 -0600

    Changed funtion

--- js/site.js
+++ js/site.js
@@ -1,5 +1,4 @@
-// TODO: Use a function closure and release global $
-$.noConflict();
+.noConflict();
 (function($){
   $(document).ready(function() {
     $('#bmi-form').on('submit', function(event) {
@@ -7,26 +6,23 @@ $.noConflict();
       var weight = $('#bmi-weight').val();
       var height = $('#bmi-height').val();
       
-      $.get(
-          'https://bmi.p.mashape.com/WyFUMDOkdrmshARoxfXDWLZmMeccp180tJEjsnCz3MCFuXJdEo' + '+weight+' 
-          + '+height+',
-          function(data,json){
-            $('#bmi').append(
-            '<p>Your BMI is: '+data.bmi.value+'</p>'
-          );
-          $('#bmi').append(
-            '<p>Your status is: '+data.bmi.status+'</p>'
-          );
-          $('#bmi').append(
-           '<p>Your risk is: '+data.bmi.risk+'</p>'
-          );
-      });  
+      $.ajax(
+        {
+          method: 'POST',
+          url: "https://bmi.p.mashape.com/",
+          headers: {
+            "X-Mashape-Key": "14EnqO5furmshplJ1g93Wydl6c7fp1unkUvjsnbVpbghxX9Nfb",
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+          },
+          data: '{"weight":{"value":"+weight+","unit":"lb"},"height":{"value":"+height+","unit":"in"},"sex":"m","age":"24","waist":"34.00","hip":"40.00"}'
+        }
+      ).done(function(data,json){
+    $('#bmi').append('<p>Your BMI is: '+data.bmi.value+'</p>');
+    $('#bmi').append('<p>Your status is: '+data.bmi.status+'</p>');
+    $('#bmi').append('<p>Your risk is: '+data.bmi.risk+'</p>');
+            
 
-        var string= 'Your Bmi: ' +value+ '. Your status:' +status+ '. Your risk:' +risk + '.';
-        $('#results').text(string);
-
-
-    });
-  event.preventDefault();
+    console.log("Done");
   });
-});(jQuery);
+});
